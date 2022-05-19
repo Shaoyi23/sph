5ea5d587171f6156cb516cfd3957a81cd6521da7
Jackys <shaoyi23@qq.com>
2022-05-19 18:46:42 +0800

    三级联动

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 //引入路由
 import router from '@/router'
-
+//三级联动组件--全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+//第一个参数:全局组件的名字
+//第二个参数:哪一个组件
+Vue.component(TypeNav.name,TypeNav)
 new Vue({
   render: h => h(App),
   //注册路由 底下的写法kv一致,则省略v
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
-我是首页
- </div>
+    <TypeNav />
+    <!-- 三级联动组件已经全局注册,不需要再引入 -->
+    <ListContainer />
+  </div>
 </template>
 
 <script>
+//引入其余组件
+import ListContainer from "@/pages/Home/ListContainer";
 export default {
-  name: 'index',
-  data() { 
-    return {
-
-    }
+  name: "",
+  components: {
+    ListContainer,
   },
-  components:{
-  },
-  methods:{
-
-  },
-  mounted() {
-
-  },
- }
+};
 </script>
 
-<style>
+<style scoped>
 </style>
