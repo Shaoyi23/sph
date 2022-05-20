04db1c16a9d73773efc92bb3efdfa0f90b581e24
Jackys <shaoyi23@qq.com>
2022-05-20 11:51:22 +0800

    推荐

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,16 +3,20 @@
     <TypeNav />
     <!-- 三级联动组件已经全局注册,不需要再引入 -->
     <ListContainer />
+    <Recommend/>
   </div>
+  
 </template>
 
 <script>
 //引入其余组件
 import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
 export default {
   name: "",
   components: {
     ListContainer,
+    Recommend,
   },
 };
 </script>
