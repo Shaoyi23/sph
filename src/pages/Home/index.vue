0d9e207f6c3c3e4d2e31d8cf410524ed36bf0125
Jackys <shaoyi23@qq.com>
2022-05-20 13:24:06 +0800

    楼层

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,20 +3,30 @@
     <TypeNav />
     <!-- 三级联动组件已经全局注册,不需要再引入 -->
     <ListContainer />
-    <Recommend/>
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <!-- 组件复用一次 -->
+    <Floor />
   </div>
-  
 </template>
 
 <script>
 //引入其余组件
 import ListContainer from "@/pages/Home/ListContainer";
 import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
 export default {
   name: "",
   components: {
     ListContainer,
     Recommend,
+    Rank,
+    Like,
+    Floor,
   },
 };
 </script>
