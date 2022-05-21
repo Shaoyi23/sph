a4d611ad802b7eef35e62ebc33c3c890e1ffc7ed
Jackys <shaoyi23@qq.com>
2022-05-22 00:52:35 +0800

    三级联动

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,24 @@
 //Home的模块仓库
-const state = {};
-const mutations = {};
-const actions = {};
+import { reqCategoryList } from "@/api";
+const state = {
+    //起始值为空数组
+    categoryList:[]
+};
+const mutations = {
+    CATEGORYLIST(state,categoryList){
+        state.categoryList = categoryList
+    }
+};
+const actions = {
+    //通过API里面的接口函数调用,向服务器发送请求,获取服务器的数据
+    //解构commit
+    async categoryList({commit}){
+        let result = await reqCategoryList();
+        if(result.code == 200){
+            commit("CATEGORYLIST",result.data)
+        }
+    }
+};
 const getters = {};
 //对外暴露
 export default{
